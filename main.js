c5db7af329bb28b54dfb714ee6c8c982cd87c64f
fujichi70 <nyq_o0@yahoo.co.jp>
2022-02-11 16:48:55 +0900

    フェードインアニメーション追加

--- main.js
+++ main.js
@@ -1,51 +1,66 @@
 // 下部アニメーション
-$(function() {
-    $('#footer').raindrops({
-        color:'rgb(66, 1, 1)',
+$(function () {
+    $("#footer").raindrops({
+        color: "rgb(66, 1, 1)",
         waveLength: 50,
         frequency: 0.5,
         waveHeight: 200,
         rippleSpeed: 0.01,
-        rippleSpeed: 0.1, 
-        density: 0.04
+        rippleSpeed: 0.1,
+        density: 0.04,
     });
 });
 
 // ハンバーガメニュー
 $(function () {
-  $('.hamburger').click(function () {
-    $(this).toggleClass('active');
+    $(".hamburger").click(function () {
+        $(this).toggleClass("active");
 
-    if ($(this).hasClass('active')) {
-      $('.sp-menu--list').addClass('active');
-    } else {
-      $('.sp-menu--list').removeClass('active');
-    }
-  });
+        if ($(this).hasClass("active")) {
+            $(".sp-menu--list").addClass("active");
+        } else {
+            $(".sp-menu--list").removeClass("active");
+        }
+    });
 });
 
 $(function () {
-  $('.main_menu li').mouseenter(function () {
-    $(this).siblings().find('ul').hide();
-    $(this).children().slideDown(200);
-  });
-  $('html').click(function () {
-    $('.pc-menu--main_menu .pc-menu--sub_menu').slideUp(200);
-  });
+    $(".main_menu li").mouseenter(function () {
+        $(this).siblings().find("ul").hide();
+        $(this).children().slideDown(200);
+    });
+    $("html").click(function () {
+        $(".pc-menu--main_menu .pc-menu--sub_menu").slideUp(200);
+    });
 });
 $(function () {
-  $('.title').on('click', function () {
-    $(this).next().slideToggle(200);
-  });
+    $(".title").on("click", function () {
+        $(this).next().slideToggle(200);
+    });
 });
 $(function () {
-  $('.item').on('inview', function () {
-    $(this).addClass('show');
-  });
+    $(".item").on("inview", function () {
+        $(this).addClass("show");
+    });
 });
 
-$(function() {
-  $(window).on('load',function() {
-    $('.main-text').addClass('appear');
-  });
+$(function () {
+    $(window).on("load", function () {
+        $(".main-text").addClass("appear");
+    });
+});
+
+// フェードイン
+$(function () {
+    $(window).scroll(function () {
+        const windowHeight = $(window).height();
+        const scroll = $(window).scrollTop();
+
+        $(".fade").each(function () {
+            const targetPosition = $(this).offset().top;
+            if (scroll > targetPosition - windowHeight + 60) {
+                $(this).addClass("in");
+            }
+        });
+    });
 });
